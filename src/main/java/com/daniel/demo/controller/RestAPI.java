9626882bac82d3d725be7b89b10ac13f9de8721a
daniel_ubuntu_house <jdanielcarranzal@hotmail.com>
2019-10-25 10:26:21 -0500

    Update function was added

--- src/main/java/com/daniel/demo/controller/RestAPI.java
+++ src/main/java/com/daniel/demo/controller/RestAPI.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.demo.Model.Alien;
@@ -59,4 +60,9 @@ public class RestAPI {
 		return response;
 	}
 	
+	@PutMapping("/alien")
+	public Alien updateAlien(Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
 }
