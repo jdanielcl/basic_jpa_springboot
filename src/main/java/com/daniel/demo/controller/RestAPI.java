89e909d9479a794b516a057832be4455cbb33f8c
daniel_ubuntu_house <jdanielcarranzal@hotmail.com>
2019-10-25 10:04:09 -0500

    post method was added

--- src/main/java/com/daniel/demo/controller/RestAPI.java
+++ src/main/java/com/daniel/demo/controller/RestAPI.java
@@ -4,29 +4,42 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.demo.Model.Alien;
 import com.daniel.demo.dao.AlienJPARepo;
 
-@Controller
+/*
+ * Avoid to use @ResponseBody Annotation and specify a rest API
+ * */
+@RestController
 public class RestAPI {
 	
 	@Autowired
 	AlienJPARepo repo;
 	
-	@RequestMapping("/aliens")
-	@ResponseBody
+	@GetMapping("/aliens")
 	public List<Alien> getAliens() {
 		return repo.findAll();
 	}
-	//@RequestMapping(path="/alien/{aid}", produces= {"application/xml"})
-	@RequestMapping(path="/alien/{aid}")
-	@ResponseBody
+	//@GetMapping(path="/alien/{aid}", produces= {"application/xml"})
+	@GetMapping(path="/alien/{aid}")
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 		return repo.findById(aid);
 	}
+	
+	/*
+	 * use (@RequestBody Alien alien) in case you want to receive data in JSON format
+	 * */
+	@PostMapping(path="/alien")
+	public Alien addAlien(Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+	
+	
+	
 }
